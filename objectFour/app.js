2320f0735645285ae24f740ab34ef352ec861f4a
Ebiladou <oyinebiladouomemu@gmail.com>
2023-05-13 22:25:48 +0100

    updated objectFour

--- objectFour/app.js
+++ objectFour/app.js
@@ -1,11 +1,16 @@
-  function Animal (name, breed, color) {
-    this.name = name;
-    this.breed = breed;
-    this.color = color;
-    this.greeting = function () {
-      console.log(`Hello, said ${this.name} the ${this.breed}.`);
-    };
-  }
+function Cat(name, breed, color) {
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.greeting = function () {
+    console.log(`Hello, said ${this.name} the ${this.breed}.`);
+  };
+}
 
-  const cat2 = new Animal ("Peaches", "Nkita", "black");
-  cat2.greeting();
+const cat1 = new Cat("Peaches", "Nkita", "black");
+cat1.greeting();
+
+const cat2 = new Cat("Spin", "German Shephard", "Grey");
+cat2.greeting();
+
+
