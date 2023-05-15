15711e15a2edae5ca6d70a1e7e5eff5e2f3ac01d
Ebiladou <oyinebiladouomemu@gmail.com>
2023-05-15 22:46:51 +0100

    updated carObj

---
+++ carObj/app.js
@@ -0,0 +1,20 @@
+const car = {
+  driving: false,
+  //status: driving,
+
+  start: function () {
+    let status = this.driving;
+    if (status === false) {
+      this.driving = true;
+    }
+  },
+
+  break: function () {
+    let status = this.driving;
+    if (status === true) {
+      this.driving = false;
+    }
+  },
+};
+
+car.start();
